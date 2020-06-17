8fef19f2b21b896f7e8aaa373a1beafb0351008d
eli10-music <elizabeth.pedro.pa@gmail.com>
2020-06-17 01:46:52 -0500

    Se agrego el README de la practica 3

--- practica3/PromedioPositivo.java
+++ practica3/PromedioPositivo.java
@@ -6,26 +6,25 @@ public class PromedioPositivo {
         double num=0;
         double prom=0;
         int cont=0;
-        double cal;
         double suma=0;
+        
         do{
             System.out.print("Ingrese las calificaciones:   ");
             num=sc.nextDouble();
-            
-            cont+=1;
-            suma=num+suma;
-            prom=suma/cont;
-           
+
+            if(num>0){
+                suma=num+suma;
+                cont+=1;
+                prom=suma/cont;
+            }
+               
         }
         while(num>0);
        
-            
-        
-       
-
+      
         System.out.print("el promedio es:  "+ prom);
-       System.out.print("cantidada de numeros:  "+ cont);
-       System.out.print("numeros:  "+ num);
+      
+       
 
        } 
     }
---
+++ practica3/README.md
@@ -0,0 +1,34 @@
+# Practica 3
+
+<h4>Ejercicio 1</h4>
+
+En el ejercicio 1 se paso de if a switch el problema tres de la pratica que se realizo en  clase.
+
+<h4>Resultados<h4>
+
+<img src="switch.jpg">
+
+<h4>Ejercicio 2</h4>
+
+ El usuario ingresa los numeros positivos y  cuando se ingresa un numero negativo se calcula el promedio. Se utilizo un ciclo do-while y un if.
+
+<h4>Resultados<h4>
+
+<img src="pos.jpg">
+
+<h4>Ejercicio 3</h4>
+
+En el ejercicio se introducen tres numero con los cuales el programa nos dira si se puede formar un triangulo o no.
+
+<h4>Resultados<h4>
+
+<img src="tri.jpg">
+
+
+
+
+
+
+
+
+
